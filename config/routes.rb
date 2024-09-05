04d44e33058feea92a8419c0956a300b93389b61
Omar Raafat <omarraafat14@gmail.com>
2024-09-06 01:17:05 +0300

    creating a new article

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+
+  # maps all of the conventional routes for articles
+  resources :articles
 end
