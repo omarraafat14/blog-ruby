3a31ac59b783d0ead85c41e9aac6285d804de746
Omar Raafat <omarraafat14@gmail.com>
2024-09-06 23:47:52 +0300

    Add secure password for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_06_194602) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_06_204131) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_06_194602) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "articles"
