4a238bd7f4afbcce9302f8930f543ec3b8e0fd72
Omar Raafat <omarraafat14@gmail.com>
2024-09-06 22:47:54 +0300

    Add one to many association between users and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  has_many :articles
   validates :username, uniqueness: { case_sensitive: false }, presence: true, length: { minimum: 3, maximum: 25 }
   validates :email, uniqueness: { case_sensitive: false }, presence: true, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 end
